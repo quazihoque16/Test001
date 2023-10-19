5fa02e51cafe82f3d4777b5600d15be874a81ad0
quazihoque16 <145925870+quazihoque16@users.noreply.github.com>
2023-10-19 10:07:47 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,11 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Line2");
-
-        System.out.println("Line4");
-        System.out.println("Line5");
-        System.out.println("L6");
-        System.out.println("L11 added");
-        System.out.println("L90");
-        System.out.println("L101 added");
-        System.out.println("L121 added");
-        System.out.println("L1221 added");
+        System.out.println("Line1........");
+        System.out.println("Line2...........");
+        System.out.println("Line3...........");
+        System.out.println("L4..............");
+         
 
     }
 }
